aac197e658d17858848281407d6f7325da5da382
Achmad Fatoni <achmadfatony@gmail.com>
2017-10-15 10:34:22 +0700

    add some hearing text

--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,16 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
+$botman->hears('hi', function ($bot) {
     $bot->reply('Hello!');
 });
+
+$botman->hears('it works', function ($bot) {
+    $bot->reply('Yea it works');
+});
+
+$botman->hears('call me {name}', function ($bot, $name) {
+    $bot->reply('Your name is: '.$name);
+});
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
